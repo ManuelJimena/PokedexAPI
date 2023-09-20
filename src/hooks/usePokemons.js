59cedb9f42732f6a044b68df066430252fed23a2
Admin <Manuel.jimena29@gmail.com>
2023-09-20 11:48:57 +0200

    refact cards.css

--- src/hooks/usePokemons.js
+++ src/hooks/usePokemons.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-const UR_DEFAULT = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+const UR_DEFAULT = "https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
 const URL_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/'
 
 function usePokemons() {
